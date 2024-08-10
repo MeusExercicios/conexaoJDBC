4261ddeaf8a65a97bbae662f961de6a1deb0da12
Vitor <vitorcostaofficial@gmail.com>
2024-08-10 10:42:03 -0300

    Acrescentei a opção de atualização de dados

--- App.java
+++ App.java
@@ -1,5 +1,5 @@
 package conexao.Banco;
-// CONEXAO, CONSULTA E INSERÇÃO NUM BANCO DE DADOS
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -10,9 +10,7 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        // URL de conexão com o banco de dados PostgreSQL
         String url = "jdbc:postgresql://localhost:5432/bancoProjeto";
-        // Nome de usuário e senha para conectar ao banco de dados
         String user = "postgres";
         String password = "admin";
 
@@ -35,27 +33,22 @@ public class App {
             System.out.println("Escolha uma opção:");
             System.out.println("1. Consultar dados");
             System.out.println("2. Inserir dados");
+            System.out.println("3. Atualizar dados");
             int choice = scanner.nextInt();
             scanner.nextLine(); // Limpa o buffer
 
             if (choice == 1) {
-                // Define a consulta SQL
                 String sql = "SELECT * FROM userbancoprojeto";
-
-                // Executa a consulta e obtém o ResultSet
                 resultSet = statement.executeQuery(sql);
 
-                // Processa o ResultSet
                 while (resultSet.next()) {
                     int id = resultSet.getInt("id");
                     String nome = resultSet.getString("nome");
                     String email = resultSet.getString("email");
 
-                    // Exibe os dados
                     System.out.println("ID: " + id + ", Nome: " + nome + ", Email: " + email);
                 }
             } else if (choice == 2) {
-                // Coleta os dados a serem inseridos
                 System.out.print("Digite o id: ");
                 int id = scanner.nextInt();
                 scanner.nextLine();
@@ -64,20 +57,38 @@ public class App {
                 System.out.print("Digite o email: ");
                 String email = scanner.nextLine();
 
-                // Define o comando SQL de inserção
                 String sql = "INSERT INTO userbancoprojeto (id, nome, email) VALUES (?, ?, ?)";
 
-                // Cria o PreparedStatement
                 PreparedStatement preparedStatement = connection.prepareStatement(sql);
                 preparedStatement.setInt(1, id);
                 preparedStatement.setString(2, nome);
                 preparedStatement.setString(3, email);
 
-                // Executa o comando SQL de inserção
                 int rowsAffected = preparedStatement.executeUpdate();
                 System.out.println("Dados inseridos com sucesso! Linhas afetadas: " + rowsAffected);
 
-                // Fecha o PreparedStatement
+                preparedStatement.close();
+            } else if (choice == 3) {
+                System.out.println("Qual id deseja atualizar ?");
+                int atualizarId = scanner.nextInt();
+                scanner.nextLine();
+
+                System.out.print("Digite o novo nome: ");
+                String novoNome = scanner.nextLine();
+
+                System.out.print("Digite o novo email: ");
+                String novoEmail = scanner.nextLine();
+
+                String sql = "UPDATE userbancoprojeto SET nome = ?, email = ? WHERE id = ?";
+
+                PreparedStatement preparedStatement = connection.prepareStatement(sql);
+                preparedStatement.setString(1, novoNome);
+                preparedStatement.setString(2, novoEmail);
+                preparedStatement.setInt(3, atualizarId);
+
+                int rowsAffected = preparedStatement.executeUpdate();
+                System.out.println("Atualizado com sucesso! Linhas afetadas: " + rowsAffected);
+
                 preparedStatement.close();
             } else {
                 System.out.println("Opção inválida.");
@@ -89,7 +100,6 @@ public class App {
             System.out.println("Erro na conexão ou na execução do comando SQL.");
             e.printStackTrace();
         } finally {
-            // Fecha o ResultSet, Statement e Connection
             try {
                 if (resultSet != null) resultSet.close();
                 if (statement != null) statement.close();
@@ -97,7 +107,6 @@ public class App {
             } catch (SQLException e) {
                 e.printStackTrace();
             }
-            // Fecha o Scanner
             scanner.close();
         }
     }
